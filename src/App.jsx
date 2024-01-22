1795a60fb355115efc18c6462b9c7a57b33e9883
Milltiades <gigakakulia2017@gmail.com>
2024-01-22 17:24:17 +0400

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 import ChildComponent from './ChildComponent';
 import { createContext } from 'react';
@@ -11,9 +11,25 @@ function App() {
     surname: 'kakulia',
   });
 
+
+  const myRef = useRef()
+
+  useEffect(() => {
+    myRef.current = 'Hello, useRef!'
+
+    setTimeout(() => {
+      console.log(myRef.current)
+      myRef.current = 'Bye Bye'
+     
+    }, 2000)
+  },[myRef])
+
+
   return (
     <MyContext.Provider value={{ user, setUser }}>
       <ChildComponent />
+  <h1>useRef Example</h1>
+
     </MyContext.Provider>
   );
 }
