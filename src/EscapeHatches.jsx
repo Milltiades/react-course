188e9758d44b5cf61fa8fa0d3bd2d60df4f36bb5
Milltiades <gigakakulia2017@gmail.com>
2024-02-02 14:36:02 +0400

    use flushSync to update the dom synchronously

--- src/EscapeHatches.jsx
+++ src/EscapeHatches.jsx
@@ -1,22 +1,42 @@
 import React from "react";
 import { useRef, useState, forwardRef,useImperativeHandle } from "react";
+import { flushSync } from 'react-dom';
 
-const MyInput = forwardRef((props, ref) => {
-    const realInputRef = useRef(null);
-  useImperativeHandle(ref, () => ({
-    // Only expose focus and nothing else
-    focus() {
-      realInputRef.current.focus();
-    },
-  }));
-    return <input {...props} ref={realInputRef} />;
-  });
+// const MyInput = forwardRef((props, ref) => {
+//     const realInputRef = useRef(null);
+//   useImperativeHandle(ref, () => ({
+//     // Only expose focus and nothing else
+//     focus() {
+//       realInputRef.current.focus();
+//     },
+//   }));
+//     return <input {...props} ref={realInputRef} />;
+//   });
 const EscapeHatches = () => {
-    const inputRef = useRef(null);
 
-    function handleClick() {
-      inputRef.current.focus();
-    }
+    const listRef = useRef(null);
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState(
+    initialTodos
+  );
+
+  function handleAdd() {
+    const newTodo = { id: nextId++, text: text };
+    flushSync(() => {
+      setText('');
+      setTodos([ ...todos, newTodo]);      
+    });
+    listRef.current.lastChild.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest'
+    });
+  }
+
+    // const inputRef = useRef(null);
+
+    // function handleClick() {
+    //   inputRef.current.focus();
+    // }
   
     // const itemsRef = useRef(null);
     // function scrollToId(itemId) {
@@ -123,12 +143,26 @@ const EscapeHatches = () => {
   return (
  
     <>
-      <>
+     <>
+      <button onClick={handleAdd}>
+        Add
+      </button>
+      <input
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+      <ul ref={listRef}>
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
+    </>
+      {/* <>
       <MyInput ref={inputRef} />
       <button onClick={handleClick}>
         Focus the input
       </button>
-    </>
+    </> */}
      {/* <nav>
         <button onClick={() => scrollToId(0)}>
           Tom
@@ -239,3 +273,13 @@ export default EscapeHatches;
 //     imageUrl: 'https://placekitten.com/250/200?image=' + i
 //   });
 // }
+
+
+let nextId = 0;
+let initialTodos = [];
+for (let i = 0; i < 20; i++) {
+  initialTodos.push({
+    id: nextId++,
+    text: 'Todo #' + (i + 1)
+  });
+}
